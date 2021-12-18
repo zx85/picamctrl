b5083e528ba25d78d392c947ec75ae2f37c79be7
StPaul's Church <stpaulsluton@gmail.com>
2021-12-18 19:28:52 +0000

    Ready for church

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from camctrl import app
 
 if __name__ == '__main__':
-#    app.run(debug=False,host='0.0.0.0',threaded=True,processes=1)
-    app.run(debug=True,host='0.0.0.0',threaded=True,processes=1)
-#    app.run(host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0',threaded=True,processes=1,port=5100)
+#    app.run(debug=True,host='0.0.0.0',threaded=True,processes=1,port=5100)
+#    app.run(host='0.0.0.0:5100')
